d51d164e3670e22120fa4c4aa43b10d860910618
runninbear5 <blakelieber@gmail.com>
2018-01-11 08:29:42 -0800

    Put world class into new projects

---
+++ SimmoneProject/PlayScreen.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PlayScreen extends World
+{
+
+    /**
+     * Constructor for objects of class StartScreen.
+     * 
+     */
+    public PlayScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1280, 769, 1); 
+    }
+}
