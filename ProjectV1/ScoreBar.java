2844285a34ed489de5512ecb881eff477aa52f7f
runninbear5 <blakelieber@gmail.com>
2018-01-12 09:18:49 -0800

    Added scorebar and changed cursor

---
+++ ProjectV1/ScoreBar.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ScoreBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScoreBar extends Actor
+{
+    /**
+     * Act - do whatever the ScoreBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
--- ProjectV1/StartScreen.java
+++ ProjectV1/StartScreen.java
@@ -1,5 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.core.WorldHandler;
+import javax.swing.*;
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Toolkit;
 /**
  * Write a description of class StartScreen here.
  * 
@@ -8,15 +13,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class StartScreen extends World
 {
-
+    JPanel Pan = WorldHandler.getInstance().getWorldCanvas();  
+    /** The new Cursor */
+    Cursor NewCursor;
+    
     /**
      * Constructor for objects of class StartScreen.
      * 
      */
     public StartScreen()
     {    
-        super(1280, 769, 1); 
+        super(1280, 769, 1, false);
+        GreenfootImage image = new GreenfootImage("crosshair.png");
+        ChangeMouseImage(image, 15,15);
         addObject(new StartButton(), 675, 400);
         addObject(new HelpButton(), 675, 300);
+    }
+    
+
+    public void act()
+    {
+        /** Sets the Cursor Image to the New Cursor */
+        Pan.setCursor(NewCursor);
+    }
+    
+    
+    /** Or you use this Method: 
+     * The image is the image, that you want the mouse to have.(The mouse has a maximum width and height for her image, if the image is too large it will be scaled on the right size)
+     * XClick and YClick is the Location on the Picture where the mouse will Click */
+    public void ChangeMouseImage(GreenfootImage image, int XClick, int yClick)
+    {
+        JPanel Panel = WorldHandler.getInstance().getWorldCanvas();
+        Cursor Cursor;
+        Toolkit Tk = Toolkit.getDefaultToolkit();
+        Point CursorPoint= new Point(XClick,yClick);
+        Cursor = Tk.createCustomCursor(image.getAwtImage(),CursorPoint,"Crosshair");
+        NewCursor = Cursor;
+        Panel.setCursor(Cursor);
     }
 }
