aee267666b9228c32b063bcfb8d9a159d923a4d6
runninbear5 <blakelieber@gmail.com>
2018-01-22 08:56:21 -0800

    fixed all issues

---
+++ Goose_Hunt_Final_Sarah_Simmone_Blake/Quit.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Quit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Quit extends Actor
+{
+    /**
+     * Act - do whatever the Quit wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.mouseClicked(this)){//if clicked on exits game
+            System.exit(0);
+        }
+    }    
+}
